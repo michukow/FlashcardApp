39dfc66426369f5a73eeac900cec02141c974753
michukow <michal.kowal.bielsko@gmail.com>
2026-01-13 23:45:29 +0100

    Added first function

--- app.py
+++ app.py
@@ -1,9 +1,53 @@
+import json
+
 class Flashcard:
     def __init__(self,side1,side2):
         self.side1=side1
+        self.side2=side2
 
     def to_dict(self):
         return self.__dict__
+
+    def info(self):
+        return self.side1, self.side2
+
+def add_flashcard():
+    while True:
+        try:
+            side1=input("Insert first side of the flashcard: ")
+            if side1=="":
+                print("The flashcard can not be empty.")
+                continue
+            break
+        except ValueError:
+            print("An error occured. Try again!")
+
+    while True:
+        try:
+            side2=input("Insert second side of the flashcard: ")
+            if side2=="":
+                print("The flashcard can not be empty.")
+                continue
+            break
+        except ValueError:
+            print("An error occured. Try again!")
+
+    flashcard=Flashcard(side1,side2)
+
+    try:
+        with open("flashcard.json","r",encoding="utf-8") as file:
+            data=json.load(file)
+            if not isinstance(data,list):
+                data=[]
+    except FileNotFoundError:
+        data=[]
+
+    data.append(flashcard.to_dict())
+
+    with open("flashcard.json","w",encoding="utf-8") as file:
+        json.dump(data,file,indent=4,ensure_ascii=False)
+
+    print(f"Flashcard {flashcard.info()} added successfully!")
 
 def main():
     while True:
@@ -16,7 +60,7 @@ def main():
         choice=str(input("Choose option: "))
 
         if choice=="1":
-            add()
+            add_flashcard()
         elif chocie=="2":
             break
         else:
